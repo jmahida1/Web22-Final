2d7fb737aa3c9e4a7cb025bef37f19f6774b4e60
jmahida1 <126614099+jmahida1@users.noreply.github.com>
2023-04-12 16:52:04 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+window.reviewData = [
+    {
+      name: "John Doe",
+      date: "2022-03-15",
+      rating: 4,
+      review: "This game is so fun and addictive! I love the graphics and gameplay."
+    },
+    {
+      name: "Jane Smith",
+      date: "2022-04-01",
+      rating: 2,
+      review: "I was really disappointed with this game. The controls were clunky and the story was boring."
+    },
+    {
+      name: "Bob Johnson",
+      date: "2022-05-10",
+      rating: 5,
+      review: "Wow, just wow! This game blew me away. The music, art, and story are all top-notch."
+    },
+    {
+      name: "Samantha Lee",
+      date: "2022-06-02",
+      rating: 3,
+      review: "This game is good, but not great. It's worth playing if you're a fan of the genre."
+    },
+    {
+      name: "Mike Chen",
+      date: "2022-07-20",
+      rating: 5,
+      review: "I can't stop playing this game! It's so well-designed and has endless replay value."
+    }
+  ];
+  
